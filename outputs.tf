f07982008858d6ef9d368094a06b0f8a05babb4b
Jozef Solmosi <jozef.solmosi@touch4it.com>
2023-06-22 16:43:13 +0200

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
 
-# output "ip_address" {
-#   value = data.aws_vpc.default
-# }
-
-
-output "task_definition_ip_address" {
-  value = data.aws_ecs_service.service_s3_manager.task_definition
+output "vpc_info" {
+   value = data.aws_vpc.default
 }
+
