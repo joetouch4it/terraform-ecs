ed5fea330629f5d0dabfdbfa78e9fa4863f0bff8
Jozef Solmosi <jozef.solmosi@touch4it.com>
2023-06-22 14:08:01 +0200

    update data

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
 data "aws_vpc" "default" {
 }
 
-data "aws_ecs_service" "service_s3_manager" {
-  service_name = aws_ecs_service.ecs_service.name
-  cluster_arn      = aws_ecs_cluster.ecs_s3_manager_cluster.arn
-}
